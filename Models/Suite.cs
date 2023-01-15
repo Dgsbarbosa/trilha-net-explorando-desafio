9c76556c5335022696b3c5a8b4b8a10c17a58d2b
DOUGLAS BARBOSA <guinho.cq@gmail.com>
2023-01-15 20:23:52 -0300

    conhecendo

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -11,8 +11,11 @@ namespace DesafioProjetoHospedagem.Models
             ValorDiaria = valorDiaria;
         }
 
+
         public string TipoSuite { get; set; }
         public int Capacidade { get; set; }
+
+        
         public decimal ValorDiaria { get; set; }
     }
 }
